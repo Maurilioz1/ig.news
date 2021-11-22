96064b5e9ddf21fcc47ad9c7e00450ef75060f48
Maurilio Lucena <maurilioz1@hotmail.com>
2021-11-22 20:11:20 -0300

    api routes

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggerIn = true;
+    const [session] = useSession();
 
-    return isUserLoggerIn ? (
-        <button type="button" className={styles.sugnInButton}>
+    console.log(session);
+
+    return session ? (
+        <button type="button" className={styles.sugnInButton} onClick={() => signOut()}>
             <FaGithub color="#38A169" />
-            Maurilio Lucena
+            {session.user.name}
             <FiX color="#718096" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.sugnInButton}>
+        <button type="button" className={styles.sugnInButton} onClick={() => signIn('github')}>
             <FaGithub color="#D69E2E" />
             Sign in with GitHub
         </button>
