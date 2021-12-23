fe50dc96fa902557496961b639e49b31084c53d5
Maurilio Lucena <maurilioz1@hotmail.com>
2021-12-22 22:05:27 -0300

    working with cms

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -9,8 +10,12 @@ export function Header() {
                 <img src="/images/logo.svg" alt="ig.news" />
 
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
